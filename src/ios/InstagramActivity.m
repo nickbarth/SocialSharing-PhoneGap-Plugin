14a51dabd8affe7cb5cdc184cf039152ea4422b1
nickbarth <nickbarth@live.com>
2015-02-11 16:21:52 -0700

    fixed syntax error

--- src/ios/InstagramActivity.m
+++ src/ios/InstagramActivity.m
@@ -44,7 +44,7 @@
       image = item;
       NSString *base64 = [UIImagePNGRepresentation(image) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
       NSURL *imageURL = [NSURL URLWithString:[@"data:image/png;base64," stringByAppendingString:base64];
-      [[UIApplication sharedApplication] openURL:imageURL]
+      [[UIApplication sharedApplication] openURL:imageURL];
     }
   }
 
