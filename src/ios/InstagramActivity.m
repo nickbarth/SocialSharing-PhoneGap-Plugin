2820208f47e79750d25072a9bb4545f2d140dcd8
nickbarth <nickbarth@live.com>
2015-02-11 21:00:19 -0700

    fixed it

--- src/ios/InstagramActivity.m
+++ src/ios/InstagramActivity.m
@@ -47,36 +47,18 @@
 }
 
 - (void)imageSavedToPhotosAlbum:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
-  NSString *escapedCaption = [self.message stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
-  NSURL *instagramURL = [NSURL URLWithString:[NSString stringWithFormat:@"instagram://library?InstagramCaption=%@", escapedCaption]];
-  [[UIApplication sharedApplication] openURL:instagramURL];
+  ALAssetsLibrary *assetLib = [[ALAssetsLibrary alloc] init];
+  [assetLib writeImageToSavedPhotosAlbum:self.image.CGImage metadata:nil completionBlock:^(NSURL *assetURL, NSError *error) {
+    NSString *escapedString = [assetURL.absoluteString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
+    NSString *escapedCaption = [message stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
+    NSURL *instagramURL = [NSURL URLWithString:[NSString stringWithFormat:@"instagram://library?AssetPath=%@&InstagramCaption=%@", escapedString, escapedCaption]];
+    [[UIApplication sharedApplication] openURL:instagramURL];
+  }];
 }
 
 - (void)performActivity
 {
   UIImageWriteToSavedPhotosAlbum(self.image, self, @selector(imageSavedToPhotosAlbum: didFinishSavingWithError: contextInfo:), nil);
-  /*
-  NSString *base64;
-  NSURL *imageURL;
-  base64 = [UIImagePNGRepresentation(image) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
-  imageURL = [NSURL URLWithString:[@"data:image/png;base64," stringByAppendingString:base64]];
-
-  [[[UIAlertView alloc] initWithTitle:@"UIAlertView" message:message delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil] show];
-  [[[UIAlertView alloc] initWithTitle:@"UIAlertView" message:[url absoluteString] delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil] show];
-  [[[UIAlertView alloc] initWithTitle:@"UIAlertView" message:[NSString stringWithFormat:@"A string: %d", counter] delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil] show];
-  */
-
-  /*
-  ALAssetsLibrary *assetLib = [[ALAssetsLibrary alloc] init];
-  [assetLib writeImageToSavedPhotosAlbum:image.CGImage metadata:nil completionBlock:^(NSURL *assetURL, NSError *error) {
-    NSString *escapedString = [assetURL.absoluteString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
-    NSString *escapedCaption = [message stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
-    NSURL *instagramURL = [NSURL URLWithString:[NSString stringWithFormat:@"instagram://library?AssetPath=%@&InstagramCaption=%@", escapedString, escapedCaption]];
-
-    [[[UIAlertView alloc] initWithTitle:@"UIAlertView" message:assetURL.absoluteString delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil] show];
-    // [[UIApplication sharedApplication] openURL:instagramURL];
-  }];
-  */
 }
 
 @end
