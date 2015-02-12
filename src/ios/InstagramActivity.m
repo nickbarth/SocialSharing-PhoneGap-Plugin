ad2d8d442e6e24f1b58c4cb3d4469f83e5bde301
nickbarth <nickbarth@live.com>
2015-02-11 17:54:30 -0700

    test

--- src/ios/InstagramActivity.m
+++ src/ios/InstagramActivity.m
@@ -4,33 +4,44 @@
 
 - (NSString *)activityType
 {
-    return @"beep.Instagram.Share";
+  return @"beep.Instagram.Share";
 }
 
 - (NSString *)activityTitle
 {
-    return @"Instagram Share";
+  return @"Instagram Share";
 }
 
 - (UIImage *)activityImage
 {
-    return [UIImage imageNamed:@"icon-40.png"];
+  return [UIImage imageNamed:@"icon-40.png"];
 }
 
 - (BOOL)canPerformWithActivityItems:(NSArray *)activityItems
 {
-    return YES;
+  return YES;
 }
 
 - (void)prepareWithActivityItems:(NSArray *)activityItems
 {
-  //self.activityItems = activityItems;
+self.activityItems = activityItems;
+}
+
+- (UIViewController*) activityViewController
+{
+  return nil;
+}
+
+- (void)performActivity
+{
   int counter = 0;
   NSString *message;
   NSURL *url;
   UIImage *image;
+  NSString *base64;
+  NSURL *imageURL;
 
-  for (id item in activityItems) {
+  for (id item in self.activityItems) {
     counter++;
     if ([item isKindOfClass:[NSString class]]) {
       message = item;
@@ -42,26 +53,17 @@
 
     if ([item isKindOfClass:[UIImage class]]) {
       image = item;
-      NSString *base64 = [UIImagePNGRepresentation(image) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
-      NSURL *imageURL = [NSURL URLWithString:[@"data:image/png;base64," stringByAppendingString:base64]];
-      if (![[UIApplication sharedApplication] openURL:imageURL]) {
-        [[[UIAlertView alloc] initWithTitle:@"UIAlertView" message:[imageURL description] delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil] show];
-      }
+      base64 = [UIImagePNGRepresentation(image) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+      imageURL = [NSURL URLWithString:[@"data:image/png;base64," stringByAppendingString:base64]];
     }
   }
 
   [[[UIAlertView alloc] initWithTitle:@"UIAlertView" message:message delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil] show];
   [[[UIAlertView alloc] initWithTitle:@"UIAlertView" message:[url absoluteString] delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil] show];
   [[[UIAlertView alloc] initWithTitle:@"UIAlertView" message:[NSString stringWithFormat:@"A string: %d", counter] delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil] show];
-}
 
-- (UIViewController*) activityViewController
-{
-    return nil;
-}
-
-- (void)performActivity
-{
+  NSURL *instagramURL = [NSURL URLWithString:@"instagram://app"];
+  [[UIApplication sharedApplication] openURL:instagramURL]
 }
 
 @end
