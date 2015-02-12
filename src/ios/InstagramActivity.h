807f2fdbd98cf4866f956acbe865494044b28e50
nickbarth <nickbarth@live.com>
2015-02-11 20:57:15 -0700

    fixed it

--- src/ios/InstagramActivity.h
+++ src/ios/InstagramActivity.h
@@ -1,5 +1,8 @@
 #import <UIKit/UIKit.h>
 
 @interface InstagramActivity : UIActivity
+@property (nonatomic, copy) NSString *message;
+@property (nonatomic, copy) NSURL *url;
+@property (nonatomic, copy) UIImage *image;
 @property (nonatomic, copy) NSArray *activityItems;
 @end
--- src/ios/InstagramActivity.m
+++ src/ios/InstagramActivity.m
@@ -25,7 +25,20 @@
 
 - (void)prepareWithActivityItems:(NSArray *)activityItems
 {
-self.activityItems = activityItems;
+  self.activityItems = activityItems;
+  for (id item in self.activityItems) {
+    if ([item isKindOfClass:[NSString class]]) {
+      self.message = item;
+    }
+
+    if ([item isKindOfClass:[NSURL class]]) {
+      self.url = item;
+    }
+
+    if ([item isKindOfClass:[UIImage class]]) {
+      self.image = item;
+    }
+  }
 }
 
 - (UIViewController*) activityViewController
@@ -33,40 +46,29 @@ self.activityItems = activityItems;
   return nil;
 }
 
+- (void)imageSavedToPhotosAlbum:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
+  NSString *escapedCaption = [self.message stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
+  NSURL *instagramURL = [NSURL URLWithString:[NSString stringWithFormat:@"instagram://library?InstagramCaption=%@", escapedCaption]];
+  [[UIApplication sharedApplication] openURL:instagramURL];
+}
+
 - (void)performActivity
 {
-  int counter = 0;
-  NSString *message;
-  NSURL *url;
-  UIImage *image;
+  UIImageWriteToSavedPhotosAlbum(self.image, self, @selector(imageSavedToPhotosAlbum: didFinishSavingWithError: contextInfo:), nil);
+  /*
   NSString *base64;
   NSURL *imageURL;
-
-  for (id item in self.activityItems) {
-    counter++;
-    if ([item isKindOfClass:[NSString class]]) {
-      message = item;
-    }
-
-    if ([item isKindOfClass:[NSURL class]]) {
-      url = item;
-    }
-
-    if ([item isKindOfClass:[UIImage class]]) {
-      image = item;
-      base64 = [UIImagePNGRepresentation(image) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
-      imageURL = [NSURL URLWithString:[@"data:image/png;base64," stringByAppendingString:base64]];
-    }
-  }
+  base64 = [UIImagePNGRepresentation(image) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+  imageURL = [NSURL URLWithString:[@"data:image/png;base64," stringByAppendingString:base64]];
 
   [[[UIAlertView alloc] initWithTitle:@"UIAlertView" message:message delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil] show];
   [[[UIAlertView alloc] initWithTitle:@"UIAlertView" message:[url absoluteString] delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil] show];
   [[[UIAlertView alloc] initWithTitle:@"UIAlertView" message:[NSString stringWithFormat:@"A string: %d", counter] delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil] show];
+  */
 
+  /*
   ALAssetsLibrary *assetLib = [[ALAssetsLibrary alloc] init];
   [assetLib writeImageToSavedPhotosAlbum:image.CGImage metadata:nil completionBlock:^(NSURL *assetURL, NSError *error) {
-    // NSURL *instagramURL = [NSURL URLWithString:@"instagram://app"];
-    // UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
     NSString *escapedString = [assetURL.absoluteString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
     NSString *escapedCaption = [message stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
     NSURL *instagramURL = [NSURL URLWithString:[NSString stringWithFormat:@"instagram://library?AssetPath=%@&InstagramCaption=%@", escapedString, escapedCaption]];
@@ -74,6 +76,7 @@ self.activityItems = activityItems;
     [[[UIAlertView alloc] initWithTitle:@"UIAlertView" message:assetURL.absoluteString delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil] show];
     // [[UIApplication sharedApplication] openURL:instagramURL];
   }];
+  */
 }
 
 @end
